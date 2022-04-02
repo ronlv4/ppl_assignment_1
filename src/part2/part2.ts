cd99c23ffbf88c35c84cedebf4a0f6ead43676d4
ronlv4 <ronlv4@gmail.com>
2022-04-02 15:39:27 +0300

    some parts work

--- src/part2/part2.ts
+++ src/part2/part2.ts
@@ -1,18 +1,90 @@
 import * as R from "ramda";
+import {identity, match, reduce, Reduced} from "ramda";
+import {returnSquaredIfFoundEven_v2} from "../part3/find";
 
 const stringToArray = R.split("");
 
 /* Question 1 */
-export const countLetters: (s: string) => {} = undefined
+export const countLetters: (s: string) => {} = R.pipe(
+    R.toLower,
+    R.match(/[a-z]/g),
+    R.countBy(R.identity)
+)
 
 /* Question 2 */
-export const isPaired: (s: string) => boolean = undefined
+export const isPaired: (s: string) => boolean = function (s: string) {
+    const parenthesesCount: { [kind: string]: number } = {
+        "(": 0,
+        "[": 0,
+        "{": 0
+    }
+    for (const c of stringToArray(s)) { // change for loop to map
+        if (c == ')') {
+            if (parenthesesCount["("]-- == 0)
+                return false
+        } else if (c == ']') {
+            if (parenthesesCount["["]-- == 0)
+                return false
+        } else if (c == '}') {
+            if (parenthesesCount["{"]-- == 0)
+                return false
+        } else if (c == '(' || c == '[' || c == '{')
+            parenthesesCount[c] += 1
+    }
+    return ((parenthesesCount["("] == 0) && (parenthesesCount["["] == 0) && (parenthesesCount["{"] == 0))
+}
 
 /* Question 3 */
-interface WordTree {
+export interface WordTree {
     root: string;
     children: WordTree[];
 }
+// export const countLetters: (s: string) => {} = R.pipe(
 
-export const treeToSentence = (t: WordTree): string => undefined
+export const treeToSentence = (t: WordTree): string => {
+    const result = t.children.map((child: WordTree) =>{
+        return treeToSentence(child)
+    })
+    return result.join(" ")
+}
+// console.log(isPaired("([{}])")) // true
+// console.log(isPaired("This is ([some]) {text}.")) // true
+// console.log(isPaired("No parens, no problems.")) // true
+// console.log(isPaired("")) // true
+// console.log(isPaired("(]")) // false
+// console.log(isPaired("This is ]some[ }text{")) // false
+// console.log(isPaired("(")) // false
+// console.log(isPaired(")(")) // false
+const children: string[] = ["y", "h", "n", "a"]
 
+const t1: WordTree = {
+    root: "Hello",
+    children: [
+        {
+            root: "students",
+            children: [
+                {
+                    root: "how",
+                    children: []
+                }
+            ]
+        },
+        {
+            root: "are",
+            children: []
+        },
+        {
+            root: "you?",
+            children: []
+        },
+    ]
+}
+export const runLengthEncoding: (s: string) => string = R.pipe(
+    stringToArray,
+    (chars: string[]) => R.groupWith(R.equals, chars),
+    R.map(x => [x[0], R.length(x)]),
+    R.map(([c, n]) => n > 1 ? `${c}${n}` : c),
+    R.join("")
+);
+
+console.log(treeToSentence(t1))
--- src/part3/find.ts
+++ src/part3/find.ts
@@ -1,4 +1,4 @@
-import { Result, makeFailure, makeOk, bind, either } from "../lib/result";
+import {Result, makeFailure, makeOk, bind, either, isOk} from "../lib/result";
 
 /* Library code */
 const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
@@ -8,7 +8,14 @@ const findOrThrow = <T>(pred: (x: T) => boolean, a: T[]): T => {
     throw "No element found.";
 }
 
-export const findResult = undefined;
+export const findResult = <T> (pred: (x: T) => boolean, a: T[]): Result<T> => {
+    try{
+        let result: T = findOrThrow(pred, a)
+        return makeOk(result)
+    }catch (e){
+        return makeFailure("no such element exist")
+    }
+}
 
 /* Client code */
 const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
@@ -20,6 +27,10 @@ const returnSquaredIfFoundEven_v1 = (a: number[]): number => {
     }
 }
 
-export const returnSquaredIfFoundEven_v2 = undefined;
+export const returnSquaredIfFoundEven_v2 = (a: number[]): Result<number> =>
+    bind(findResult(x => x % 2 === 0, a), x => makeOk(x * x));
 
-export const returnSquaredIfFoundEven_v3 = undefined;
+export const returnSquaredIfFoundEven_v3 = (a: number[]): number =>
+either(findResult(x => x % 2 === 0, a), x => x * x, _ => -1);
+
+
